e267d4a0e9458b28529d34872b281a83ee6ea38b
Ovi Grama <gramaovi@gmail.com>
2021-11-02 21:11:28 +0200

    Add project files.

---
+++ PayParking/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PayParking
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
