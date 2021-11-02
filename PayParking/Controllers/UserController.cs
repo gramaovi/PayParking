39e9bad23474c4a9901a976e9f2953d3da2349f1
Ovi Grama <gramaovi@gmail.com>
2021-11-02 21:56:44 +0200

    UserController created

---
+++ PayParking/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using PayParking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PayParking.Controllers
+{
+    public class UserController : Controller
+    {
+       //register
+       [HttpGet]
+       public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude ="IsEmailVerified,ActivationCode")]User user)
+        {
+            return View(user);
+        }
+    }
+}
