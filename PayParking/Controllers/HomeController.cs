b98e76761dfd51ec02defcac2055cffcc54f8522
Ovi Grama <gramaovi@gmail.com>
2021-11-03 08:56:53 +0200

    Park checkin working

--- PayParking/Controllers/HomeController.cs
+++ PayParking/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace PayParking.Controllers
 {
@@ -24,7 +25,45 @@ namespace PayParking.Controllers
                 return View(de.Parkings.ToList());
             }
         }
+        public ActionResult ParkCar(int? id)
+       
+        {
+            using (DatabaseEntities2 de = new DatabaseEntities2())
+            {
+                if (!id.HasValue)
+                    return HttpNotFound();
 
+                Parking park = de.Parkings.Find(id);
+                if (null == park)
+                {
+                    return HttpNotFound();
+                }
+                return View(park);
+
+
+            }
+        }
+        [HttpPost]
+        public ActionResult ParkCar(Parking park)
+        {
+            string email=System.Web.HttpContext.Current.User.Identity.Name;
+
+            using (DatabaseEntities1 de = new DatabaseEntities1())
+            {
+
+            }
+            using (DatabaseEntities2 de = new DatabaseEntities2())
+            {
+                
+                park.CheckIn= DateTime.Now;
+                park.IsFree = false;
+                de.Entry(park).State = System.Data.Entity.EntityState.Modified;
+                de.SaveChanges();
+                return RedirectToAction("GetParkings");
+
+
+            }
+        }
 
     }
 }
